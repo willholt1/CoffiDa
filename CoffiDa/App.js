7772bf652c44f76067b02c44136eade6ccc67071
willholt1 <will.holt.xyz@gmail.com>
2021-02-15 18:44:17 +0000

    testing github

--- CoffiDa/App.js
+++ CoffiDa/App.js
@@ -13,7 +13,7 @@ class Coffi extends Component {
   render(){
     return(
       <View>
-        <Text>Hello World</Text>
+        <Text>Hello World adding words as a test</Text>
       </View>
     );
   }
