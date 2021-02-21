b3bee46ff087abf887a24dfde66d2d0ba43205a2
willholt1 <will.holt.xyz@gmail.com>
2021-02-21 23:29:47 +0000

    created files for all screens

--- CoffiDa/App.js
+++ CoffiDa/App.js
@@ -3,6 +3,7 @@ import 'react-native-gesture-handler';
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+im
 
 import HomeScreen from './Components/HomeScreen';
 import Location from './Components/Location'
---
+++ CoffiDa/Components/AccountInfo.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class AccountInfo extends Component{
+  render(){
+    return(
+        <View>
+          <Text>Account Info</Text>
+        </View>
+    );
+  }
+}
+
+export default AccountInfo;
